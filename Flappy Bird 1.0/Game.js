307dca746ccf712df89bb8158859ecfb4e8fa4f2
afra666 <48674984+afra666@users.noreply.github.com>
2019-10-12 11:45:38 +0800

    Update Game.js
    修改适配、碰撞检测

--- Flappy Bird 1.0/Game.js
+++ Flappy Bird 1.0/Game.js
@@ -52,11 +52,13 @@ let pipeW=gameW*0.1;
 let gap=birdH*5;
 //first pipe
 let pipeArr=[];
+// 向第一根管道push左上角坐标
 pipeArr[0]={
     pX0:gameW,
     pY0:-gameH*0.5
 };
 /*pY1:pipeArr[0].pY0+pipeLen+birdH*3*/
+// 加载图片资源、产生随机颜色鸟、显示欢迎界面
 function loadData() {
     mediaScore.src="media/score.mp3";
     mediaSwing.src="media/swing.mp3";
@@ -79,7 +81,7 @@ function welcome() {
     ctx.drawImage(imgGround,groundX,groundY,groundW,groundH);
     ctx.drawImage(imgWelcome,0,0,gameW,gameH);
     ctx.fillText("Highest Score:"+score,10,30);
-
+    // 20毫秒更换一张图片setInterval方法
     let timer=setInterval(function () {
         ctx.drawImage(imgBird[fno%100%3],birdX,birdY,birdW,birdH);
         fno++;
@@ -87,12 +89,16 @@ function welcome() {
             fno=0;
         }
     },20);
+    // onclick事件触发游戏开发
     document.onclick=function () {
         mediaSwing.play();
         mediaScore.play();
-        document.onclick=null;
-        clearInterval(timer);
+        document.onclick=null;//清除onclick事件
+        clearInterval(timer);//清除计数器
         ctx.clearRect(0,0,gameW,gameH);
+        /**
+         * 开始游戏并添加事件,按下键盘+点击鼠标+触摸屏幕
+         */
         start();
         document.addEventListener("click",birdUp);
         document.addEventListener("keydown",birdUp);
@@ -105,33 +111,36 @@ function start() {
     drawBird();
     drawPipe();
     drawGround();
-
+    // windows.requestAnimationFrame重绘画布更新动画
     requestAnimationFrame(start);
 }
 function smartScale() {
-    //ip5:[320*568],ip8P[414*736],ipX:[375*812]
-    //cW[320-414],cH[568-812]
+    //ip4:[320*480],Nexus 7[600*960]
+    //cW[320-600],cH[480-960]
     let cW=document.documentElement.clientWidth;
     let cH=document.documentElement.clientHeight;
     if (cW<320) {
         cW=320;
-    }else if(cW>414){
-        cW=414;
+    }else if(cW>600){
+        cW=600;
     }
-    if (cH<568) {
-        cH=568;
-    }else if(cH>812){
-        cH=812
+    if (cH<480) {
+        cH=480;
+    }else if(cH>960){
+        cH=960
     }
     return {
         gameW:cW,
         gameH:cH
     }
 }
+// 随机颜色的鸟，三个动作图片
 function randomBirdColor() {
     for (let i = 0; i < 3; i++) {
+        // 令imgBird[i]数据类型为三个Image型
         imgBird[i]=new Image();
     }
+    // 随机黄蓝红三种颜色
     switch (rdmNumB) {
         case 0:
             imgBird[0].src="img/bird0_0.png";
@@ -153,7 +162,7 @@ function randomBirdColor() {
     }
 }
 function drawBg() {
-    //final
+    //向左运动x坐标减少
     bgX-=pipeSpeed;
     if (bgX==-gameW) {
         bgX=0;
@@ -169,13 +178,13 @@ function drawBg() {
 }
 function drawPipe() {
     for (let i = 0; i < pipeArr.length; i++) {
-        //for
+        //画出上下管道
         ctx.drawImage(imgPipeup,pipeArr[i].pX0,pipeArr[i].pY0,pipeW,pipeLen);
         ctx.drawImage(imgPipedown,pipeArr[i].pX0,pipeArr[i].pY0+pipeLen+gap,pipeW,pipeLen);
         console.log(pipeArr.length);
         gameOverJudge(pipeArr[i]);
 
-        //new  rdmY属于区间[-300,0]
+        //如果最后一根管道x值为200px，push新管道
         if (pipeArr[i].pX0===200) {
 
             pipeArr.push({
@@ -186,6 +195,7 @@ function drawPipe() {
         }
         pipeArr[i].pX0-=pipeSpeed;
     }
+    // 显示分数
     ctx.fillText("Highest Score:"+score,10,30);
 }
 function drawGround() {
@@ -197,16 +207,18 @@ function drawGround() {
     ctx.drawImage(imgGround,groundX+gameW,groundY,groundW,groundH);
 }
 function drawBird() {
+    // 每一毫秒fno加一
     (++fno)===1000?fno:0;
-    //for
+    //imgBird[fno%300%3]取值区间[0-299%3][0,1,2],减慢图片更换的速度的一种方法
     ctx.drawImage(imgBird[fno%300%3],birdX,birdY,birdW,birdH);
     birdY+=birdSpeedDown;
     t++;
+    // 为了增加游戏体验，更改重力值
     birdSpeedDown=(2*t+1)*0.1;
 }
 function birdUp() {
     t=0;
-    mediaSwing.play();
+    mediaSwing.play();//播放翅膀扇动声音
     birdY-=birdSpeedUp;
 }
 function gameOverJudge(nowPipe) {
@@ -228,10 +240,12 @@ function gameOverJudge(nowPipe) {
         mediaScore.play();
         score+=10;
     }
-    if(
-        (birdFoot>groundHead+10)||
-        ((birdRight>pipeUpLeft&&birdLeft<pipeUpRight)&&(birdHead<pipeUpFoot||birdFoot>pipeDownHead))
-            ){
+
+//碰撞检测
+    if((birdFoot>groundHead)
+        ||(birdRight>pipeUpLeft&&birdHead<pipeUpFoot&&birdLeft<pipeUpRight)
+        ||(birdFoot>pipeDownHead&&birdRight>pipeDownLeft&&birdLeft<pipeDownRight)
+    ){
         alertFlag++;
         if (alertFlag===1) {
             alert("Game Over\nYour score is :"+score);
